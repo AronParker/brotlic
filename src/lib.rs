f3201a5eba0b4dadcdeff5db68503879ba629fed
Aron Parker <hl3mukkel@gmail.com>
2022-04-05 23:49:32 +0200

    Fix a spelling mistake in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Brotlic
 //!
-//! Brotlic (or BrotlyC) is a thin wrapper around [brotli](https://github.com/google/brotli). It
+//! Brotlic (or BrotliC) is a thin wrapper around [brotli](https://github.com/google/brotli). It
 //! provides Rust bindings to all compression and decompression APIs. On the fly compression and
 //! decompression is supported for both `BufRead` and `Write` via [`CompressorReader<R>`,
 //! `CompressorWriter<W>`, `DecompressorReader<R>` and `DecompressorWriter<W>`. For low-level
